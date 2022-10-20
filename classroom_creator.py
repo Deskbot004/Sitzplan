d9328be682a9a5f7f53461f06a3fae58d992e8ab
Dirk D <joshua.berger@rocketmail.com>
2022-10-20 18:23:17 +0200

    update classroom_creator

--- classroom_creator.py
+++ classroom_creator.py
@@ -1,13 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import json
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def hello():
     return render_template("create_classroom.html")
 
 
+@app.route("/ProcessUserInfo<string:userinfo>", methods=["POST"])
+def ProcessUserInfo(userinfo):
+    userinfo = json.loads(userinfo)
+    print()
+    print(userinfo)
+    print()
+    return "/"
+
+
 if __name__ == "__main__":
-        app.run()
+        app.run(debug=True)
 
