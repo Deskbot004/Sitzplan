4cf81b9fcf9ab63ce36d2283be0854e1dd0f5816
Deskbot004 <115943461+Deskbot004@users.noreply.github.com>
2022-10-20 18:21:46 +0200

    Stubs for Websites implemented

---
+++ StartWeb.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return render_template("startsite.html")
+
+
+@app.route('/home')
+def hello6():
+    return render_template("startsite.html")
+
+
+@app.route('/classrooms')
+def hello2():
+    return render_template("create_classroom.html")
+
+
+@app.route('/seating')
+def hello3():
+    return render_template("seating.html")
+
+
+@app.route('/students')
+def hello4():
+    return render_template("students.html")
+
+
+@app.route('/about')
+def hello5():
+    return render_template("about.html")
+
+
+if __name__ == "__main__":
+        app.run()
+
