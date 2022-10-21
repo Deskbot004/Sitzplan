b06e164dce00010ea68193cbc28fd06211f9656d
Deskbot004 <115943461+Deskbot004@users.noreply.github.com>
2022-10-21 19:43:30 +0200

    Added common.js for common functions

---
+++ static/js/common.js
@@ -0,0 +1,19 @@
+function switchToClassroom() {
+    window.location = "classrooms";
+}
+
+function switchToHome() {
+    window.location = "home";
+}
+
+function switchToSeating() {
+    window.location = "seating";
+}
+
+function switchToStudents() {
+    window.location = "students";
+}
+
+function switchToAbout() {
+    window.location = "about";
+}
