8693d3ab1696fd9889375425c1d5e796a96ab42c
Deskbot004 <115943461+Deskbot004@users.noreply.github.com>
2022-10-22 15:46:56 +0200

    sync

--- StartWeb.py
+++ StartWeb.py
@@ -51,8 +51,8 @@ def testfn():
     if request.method == "POST":
         print(request.form["result"])
         print(students.get_student_list(request.form["result"]))
-        #return students.get_student_list(request.form["result"])
-        return render_template("students_list.html")
+        return students.get_student_list(request.form["result"])
+        #return render_template("students_list.html")
     elif request.method == "GET":
         return students.get_all_student_lists()
 
