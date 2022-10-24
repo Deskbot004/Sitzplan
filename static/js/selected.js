d2cf127a83b0e14eb35ed7b21d506f0ae7e23f58
Deskbot004 <115943461+Deskbot004@users.noreply.github.com>
2022-10-24 16:28:20 +0200

    ask_delete relocated for later usage

--- static/js/selected.js
+++ static/js/selected.js
@@ -8,3 +8,16 @@ function start(){
     document.getElementById('head_text').innerHTML = document.getElementById('head_text').innerHTML.replace("free", data);
     document.getElementById('filename').value = data;
 };
+
+/*
+	Asks the user if he really wants to delete the data which was selected.
+
+	@return: void
+*/
+function ask_delete() {
+	var ask_text = "Are you sure that you want to delete free?";
+	ask_text = ask_text.replace("free", data);
+	if(confirm(ask_text)) {
+		deleteInformation();
+	}
+}
